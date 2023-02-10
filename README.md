5ee50a842d3e4d7d37b3d902ad29d8ddda3d6f8a
acdevfullstack <ac.devfullstack@gmail.com>
2023-02-10 00:00:15 -0300

    Atualizado com sucesso.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 Código Fonte, com vários exemplos, para fim de estudos.
+Obs.: Todos projetos, são atualizados na versão .Net 6.0
---
+++ calcpagfunc/Entities/OutsourcedEmployee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace calcpagfunc.Etities
+{
+     class OutsourcedEmployee : Employee
+    {
+    public double AdditionalCharge { get; set; }
+
+        //Construtor
+        public OutsourcedEmployee() { 
+        
+        }
+        
+        public OutsourcedEmployee(string name, int hours, double valuePerHour, double additionalCharge) 
+            : base(name, hours, valuePerHour)
+        {
+            AdditionalCharge = additionalCharge;
+        }
+
+        //Obs.: Aproveita o campo Payment
+        public override double Paymemt()
+        {
+            return base.Paymemt() + 1.1 * AdditionalCharge;
+        }
+
+    }
+}
